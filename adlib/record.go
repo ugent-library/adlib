02b5639bb481f11f2ad24413d41720cf7c1d94bb
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2021-05-03 19:33:55 +0200

    snakecase json

--- adlib/record.go
+++ adlib/record.go
@@ -1,22 +1,22 @@
 package adlib
 
 type Record struct {
-	PriRef                string          `xml:"priref,omitempty" json:"priRef,omitempty"`
+	PriRef                string          `xml:"priref,omitempty" json:"priref,omitempty"`
 	Institution           *Institution    `xml:"institution.name,omitempty" json:"institution,omitempty"`
 	Collection            string          `xml:"collection>term,omitempty" json:"collection,omitempty"`
-	ObjectCategory        string          `xml:"object_category>term,omitempty" json:"objectCategory,omitempty"`
-	ObjectNumber          string          `xml:"object_number,omitempty" json:"objectNumber,omitempty"`
-	ObjectName            string          `xml:"Object_name>object_name>term,omitempty" json:"objectName,omitempty"`
+	ObjectCategory        string          `xml:"object_category>term,omitempty" json:"object_category,omitempty"`
+	ObjectNumber          string          `xml:"object_number,omitempty" json:"object_number,omitempty"`
+	ObjectName            string          `xml:"Object_name>object_name>term,omitempty" json:"object_name,omitempty"`
 	Title                 string          `xml:"Title>title,omitempty" json:"title,omitempty"`
 	Description           string          `xml:"Description>description,omitempty" json:"description,omitempty"`
 	Production            *Production     `xml:"Production,omitempty" json:"production,omitempty"`
-	ProductionPeriod      []string        `xml:"production.period>term,omitempty" json:"productionPeriod,omitempty"`
-	ProductionDate        *ProductionDate `xml:"Production_date,omitempty" json:"productionDate,omitempty"`
+	ProductionPeriod      []string        `xml:"production.period>term,omitempty" json:"production_period,omitempty"`
+	ProductionDate        *ProductionDate `xml:"Production_date,omitempty" json:"production_date,omitempty"`
 	Dimension             []Dimension     `xml:"Dimension,omitempty" json:"dimension,omitempty"`
 	Material              []string        `xml:"Material>material>term,omitempty" json:"material,omitempty"`
 	Taxonomy              *Taxonomy       `xml:"Taxonomy,omitempty" json:"taxonomy,omitempty"`
-	FieldCollPlace        []string        `xml:"field_coll.place>term,omitempty" json:"fieldCollPlace,omitempty"`
-	FieldCollPlaceFeature []string        `xml:"field_coll.place.feature>term,omitempty" json:"fieldCollPlaceFeature,omitempty"`
+	FieldCollPlace        []string        `xml:"field_coll.place>term,omitempty" json:"field_coll_place,omitempty"`
+	FieldCollPlaceFeature []string        `xml:"field_coll.place.feature>term,omitempty" json:"field_coll_place_feature,omitempty"`
 }
 
 // TODO address.place,institution_code
@@ -33,8 +33,8 @@ type Production struct {
 // TODO biography
 type Creator struct {
 	Name      string `xml:"name,omitempty" json:"name,omitempty"`
-	BirthDate string `xml:"birth.date.start,omitempty" json:"birthDate,omitempty"`
-	DeathDate string `xml:"death.date.start,omitempty" json:"deathDate,omitempty"`
+	BirthDate string `xml:"birth.date.start,omitempty" json:"birth_date,omitempty"`
+	DeathDate string `xml:"death.date.start,omitempty" json:"death_date,omitempty"`
 }
 
 // TODO production.date.start.prec, production.date.end.prec
@@ -52,7 +52,7 @@ type Dimension struct {
 }
 
 type Taxonomy struct {
-	CommonName string       `xml:"taxonomy.common_name,omitempty" json:"commonName,omitempty"`
+	CommonName string       `xml:"taxonomy.common_name,omitempty" json:"common_name,omitempty"`
 	Rank       TaxonomyRank `xml:"taxonomy.rank,omitempty" json:"rank,omitempty"`
 }
 
